b5b80dbf2547ddc488fe34f92639f213ddbf4e18
Bogunov Ilya <bogunov@gmail.com>
2014-07-19 13:09:57 +0400

    removing unused code

--- public/js/helpers/pluralize.js
+++
@@ -1,8 +0,0 @@
-/* global Ember */
-
-Ember.Handlebars.helper('pluralize', function (singular, count) {
-    /* From Ember-Data */
-    var inflector = new Ember.Inflector(Ember.Inflector.defaultRules);
-
-    return count === 1 ? singular : inflector.pluralize(singular);
-});
